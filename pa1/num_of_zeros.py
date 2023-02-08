3bda1543b3f2fdc980ae6aca95301a02618aec1c
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2023-02-07 17:16:08 -0800

    Programming Assignment 1 Part A
    A 2-dimensional grid (or nested list) of 0s and 1s sorted in non-increasing  (descending order -- largest to smallest) order both row-wise and column-wise. Write a function that takes the grid as an input and returns the number of zeros in the grid. Assume that each list in the nested list has the same length (as in matrices).

---
+++ pa1/num_of_zeros.py
@@ -0,0 +1,12 @@
+def num_of_zeros(grid: list) -> int:
+    """Counts the number of zeroes by counting every time 0 appears in each row using the .count() method"""
+    count = 0  # initializing count variable
+    for list_ in grid:  # looping through every row in the grid
+        count += list_.count(0)  # for each row in the grid, the amount of times 0 occurs is added to variable count
+    return count  # after all rows are looped through, the total count of 0s is returned
+  
+    """Test Code:"""
+    # assert num_of_zeros([[1,1,1,0],[1,1,1,0],[1,1,0,0],[0,0,0,0]]) == 8
+    # assert num_of_zeros([[1, 1],[1, 1]]) == 0
+    # assert num_of_zeros([[],[]]) == 0
+    # assert num_of_zeros([[], [0, 0, 1]]) == 2
